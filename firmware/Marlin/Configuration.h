e0618d1f73b1db901992017a2634b8def8d40721
Grégory Caltagirone <numidark@gmail.com>
2016-11-16 23:23:02 +0100

    Printer name correctly affected
    Now "XL300 ready"

--- firmware/Marlin/Configuration.h
+++ firmware/Marlin/Configuration.h
@@ -86,7 +86,7 @@
 #endif
 
 // Define this to set a custom name for your generic Mendel,
-// #define CUSTOM_MENDEL_NAME "This Mendel"
+#define CUSTOM_MENDEL_NAME "XL300"
 
 // Define this to set a unique identifier for this printer, (Used by some programs to differentiate between machines)
 // You can use an online service to generate a random UUID. (eg http://www.uuidgenerator.net/version4)
